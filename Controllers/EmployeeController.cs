7797628987fba0458ed9c06dd0fd7162e23bc15e
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-11-01 22:27:06 +0300

    EmployeeController and  3 endpoint created.(GetAllEmployee, GetOneEmployee and AddEmployee)

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Entities.Models;
+using CompanyEmployees.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
 {
@@ -6,10 +8,31 @@ namespace CompanyEmployees.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly EmployeeRepo _repository = new EmployeeRepo();
 
-        
+        [HttpGet]
+        public ActionResult <IEnumerable<Employee>> GetAllEmployee()
+        {
+            var items  =  _repository.GetAllEmployee();
 
+            return Ok(items);
+        }
 
+        [HttpGet("{RecordNo}")]
+        public ActionResult <Employee> GetOneEmployee(string RecordNo)
+        {
+            var item = _repository.GetOneEmployeeByRecordNo(RecordNo);
+            
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> AddEmployee(Employee employee)
+        {
+            _repository.AddEmployee(employee);
+
+            return Ok();
+        }
 
     }
 }
