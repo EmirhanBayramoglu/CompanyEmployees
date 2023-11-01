db3ee7240c7bdc5a41cfe482e033f678529155e2
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-11-01 20:16:41 +0300

    Empty EmployeeRepo class created.

---
+++ Repositories/EmployeeRepo.cs
@@ -0,0 +1,27 @@
+﻿using CompanyEmployees.Entities.Models;
+
+namespace CompanyEmployees.Repositories
+{
+    public class EmployeeRepo : IEmployeeRepo
+    {
+        public void AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetOneEmployeeByRecordNo(string RecordNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
