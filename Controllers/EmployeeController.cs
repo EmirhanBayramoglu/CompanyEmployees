f4b46075c9c9a9fa8fdd269014a62a215ef5c965
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-11-01 21:11:43 +0300

    EmployeeController created.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        
+
+
+
+    }
+}
