1bde0f962d8ecb7a4e458b7b49425c0a22043944
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-11-01 19:54:11 +0300

    Employee properties created.

--- Entities/Models/Employee.cs
+++ Entities/Models/Employee.cs
@@ -1,6 +1,14 @@
-﻿namespace CompanyEmployees.Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyEmployees.Entities.Models
 {
     public class Employee
     {
+        [MaxLength(11, ErrorMessage = "Recorn no has to be 11 character.")]
+        [MinLength(11, ErrorMessage = "Recorn no has to be 11 character.")]
+        public string RecordNo { get; set; }
+        public string Lname { get; set; }
+        public string Fname { get; set; }
+
     }
 }
