b25f6b802ebec63092d5344147b65aedfa0984d6
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-11-01 23:25:39 +0300

    EmployeeContext created, and started to DbContext part.

---
+++ Repositories/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using CompanyEmployees.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployees.Repositories
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> opt) : base(opt) 
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
