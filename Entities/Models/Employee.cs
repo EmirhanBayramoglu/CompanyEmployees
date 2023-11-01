e66f2a11bf927d2e6e7696edc7c34de7ee830cd5
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-11-01 20:10:43 +0300

    LowerEmployee and UpperEmployee properties added to Employee model. Repositories folder and IEmployeeRepo interface created.

--- Entities/Models/Employee.cs
+++ Entities/Models/Employee.cs
@@ -4,11 +4,14 @@ namespace CompanyEmployees.Entities.Models
 {
     public class Employee
     {
+        [Key]
         [MaxLength(11, ErrorMessage = "Recorn no has to be 11 character.")]
         [MinLength(11, ErrorMessage = "Recorn no has to be 11 character.")]
         public string RecordNo { get; set; }
         public string Lname { get; set; }
         public string Fname { get; set; }
+        public string UpperEmployee { get; set; }
+        public string LowerEmployee { get; set; }
 
     }
 }
